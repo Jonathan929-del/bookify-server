4cc467d12936d2176ad68b43b7c6a4814a82a1bc
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2025-09-27 01:25:18 +0300

    Updated package.json file

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -32,3 +32,21 @@ export class UserController {
     return this.userService.remove(+id);
   }
 }
+
+
+
+
+// "scripts": {
+//   "build": "tsc -p tsconfig.build.json",
+//   "format": "prettier --write \"src/**/*.ts\" \"test/**/*.ts\"",
+//   "start": "node dist/main.js",
+//   "start:dev": "nest start --watch",
+//   "start:debug": "nest start --debug --watch",
+//   "start:prod": "node dist/main.js",
+//   "lint": "eslint \"{src,apps,libs,test}/**/*.ts\" --fix",
+//   "test": "jest",
+//   "test:watch": "jest --watch",
+//   "test:cov": "jest --coverage",
+//   "test:debug": "node --inspect-brk -r tsconfig-paths/register -r ts-node/register node_modules/.bin/jest --runInBand",
+//   "test:e2e": "jest --config ./test/jest-e2e.json"
+// }
