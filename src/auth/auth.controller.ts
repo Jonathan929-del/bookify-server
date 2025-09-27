335bb2947a2114b4fee4a504d1b6f80ee09c3d93
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2025-09-27 03:37:10 +0300

    Fixed http cookies issue

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -30,7 +30,7 @@ export class AuthController {
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             path:'/',
             maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
         });
@@ -60,7 +60,7 @@ export class AuthController {
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             path:'/',
             maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
         });
